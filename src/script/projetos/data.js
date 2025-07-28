c99408af841bb96561a762793f17a108905c45c3
Atila Araujo <atilabh96@gmail.com>
2025-07-28 17:01:58 -0400

    feat: inicio do styles e funcionalidade dos cards de cada projeto na sessoã 3

---
+++ src/script/projetos/data.js
@@ -0,0 +1,46 @@
+export const projetos = {
+    simuladorOS: {
+        nome: 'EimiOS',
+        img: '',
+        linkVercel: 'https://simulador-de-os-y38g-iogz65fl1-atilas-projects-23f305cf.vercel.app',
+        linkGitHub: '',
+        tecnologia: [
+            'JavaScript',
+            'Css'
+        ],
+        descricao: ''
+    },
+    geradorRpg: {
+        nome: 'Gerador RPG',
+        img: '',
+        linkVercel: 'https://gerador-rpg.vercel.app/',
+        linkGitHub: '',
+        tecnologia: [
+            'JavaScript',
+            'Html',
+            'Css'
+        ],
+        descricao: ''
+    },
+    atlasNote: {
+        nome: 'Atlas Note',
+        img: '',
+        linkVercel: 'https://atlas-note-phi.vercel.app/',
+        linkVercel: '',
+        tecnologia: [
+            'JavaScript',
+            'Css'
+        ],
+        descricao: ''
+    },
+    terminalRpg: {
+        nome: 'Terminal RPG',
+        img: '',
+        linkDeploy: 'https://atylla.github.io/TerminalRPG/',
+        linkGitHub: 'https://github.com/Atylla/TerminalRPG',
+        tecnologia: [
+            'JavaScript'
+        ],
+        descricao: ''
+    }
+}
