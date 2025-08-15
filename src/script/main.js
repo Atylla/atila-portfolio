77eb3dfaa2e2c612042ac0a773eb536f00a9148e
Atila Araujo <atilabh96@gmail.com>
2025-08-15 00:11:59 -0400

    fix: funcionao formulario adicionado a main.js

--- src/script/main.js
+++ src/script/main.js
@@ -2,6 +2,7 @@ import { loadingInitial } from "./inicial.js/loading.js";
 import { navBar, toggleInterruptor } from "./navegacao/nav-bar.js";
 import { cardsProjetos } from "./projetos/cards.js";
 import { btnCircle } from "./sections/circle.js";
+import { formulario } from "./sections/contato.js";
 import { sectionFocus } from "./sections/section.js";
 import { skills } from "./sections/skills.js";
 import { verProjetos } from "./sections/ver-projeots.js";
@@ -18,4 +19,5 @@ document.addEventListener("DOMContentLoaded", () => {
     cardsProjetos();
     sectionFocus();
     toggleInterruptor();
+    formulario();
 });
