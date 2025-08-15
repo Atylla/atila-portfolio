4b9c3e99869928e732b180c7d8c93f9fed4da5a2
Atila Araujo <atilabh96@gmail.com>
2025-08-14 21:32:34 -0400

    feat: agora quando a section esta em foco, o botao correspondente a ela adquire styles

--- src/script/sections/section.js
+++ src/script/sections/section.js
@@ -2,8 +2,16 @@ export const sectionFocus = () => {
     const observer = new IntersectionObserver(
         (entries) => {
             entries.forEach(entry => {
+                const id = entry.target.getAttribute('id');
+                const navButton = document.querySelector(`.nav-btn[data-nav="${id}"]`);
+
+
                 if (entry.isIntersecting) {
                     entry.target.classList.add('middle');
+                    if(navButton) {
+                        document.querySelectorAll('.nav-btn').forEach(btn => btn.classList.remove('active'));
+                        navButton.classList.add('active');
+                    }
                 } else {
                     entry.target.classList.remove('middle');
                 }
